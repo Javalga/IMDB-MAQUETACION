5e05aff720207568b38ca8ef282cc9c31834433c
Javier Alonso <jalonsogal@gmail.com>
2022-11-08 15:03:48 +0100

    proyecto terminado

--- classes/movie/movie.js
+++ classes/movie/movie.js
@@ -1,4 +1,4 @@
-export class Movie {
+class Movie {
   constructor(
     title,
     releaseYear,
@@ -9,7 +9,7 @@ export class Movie {
     producer,
     writer,
     language,
-    plataforma,
+    plataform,
     isMCU,
     distributor,
     mainCharacterName,
@@ -22,7 +22,7 @@ export class Movie {
     this.director = director;
     this.writer = writer;
     this.language = language;
-    this.plataforma = plataforma;
+    this.plataform = plataform;
     this.isMCU = isMCU;
     this.mainCharacterName = mainCharacterName;
     this.producer = producer;
@@ -37,11 +37,10 @@ export class Movie {
     console.log("Release Year - " + this.releaseYear);
     console.log("Nacionality - " + this.nacionality);
     console.log("Genre- " + this.genre);
-    console.log("Actors - " + this.actors);
     console.log("Director - " + this.director);
     console.log("Writer - " + this.writer);
     console.log("Language - " + this.language);
-    console.log("Plataform - " + this.plataforma);
+    console.log("Plataform - " + this.plataform);
     console.log("Is MCU? - " + this.isMCU);
     console.log("Main Character Name - " + this.mainCharacterName);
     console.log("Producer - " + this.producer);
